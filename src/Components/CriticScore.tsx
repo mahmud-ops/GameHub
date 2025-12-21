27df94cac61210502468ee053d1d4bde92186557
Mahmud <abdullahmahmud01798@gmail.com>
2025-12-22 01:25:23 +0600

    Displaying critic score

---
+++ src/Components/CriticScore.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+
+const CriticScore = ({ score }: Props) => {
+    const color = score > 90 ? 'green' : score > 80 ? 'yellow' : 'orange';
+  return <Badge colorScheme={color} fontSize={'15px'} borderRadius={'5px'} paddingX={2}>{score}</Badge>;
+};
+
+export default CriticScore;
