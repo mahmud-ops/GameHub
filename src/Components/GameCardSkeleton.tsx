b13ffbb077c2c0162a978e8574b01d8b56ccf1dc
Mahmud <abdullahmahmud01798@gmail.com>
2025-12-22 21:58:34 +0600

    changed logo

---
+++ src/Components/GameCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react"
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width={'300px'} borderTopRadius={5} overflow={'hidden'}>
+        <Skeleton height={'230px'}/>
+
+        <CardBody>
+            <SkeletonText/>
+        </CardBody>
+    </Card>
+  )
+}
+
+export default GameCardSkeleton
--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,22 +1,29 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../Hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 export default function GameGrid() {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <>
       {error && <Text color="pink">{error}</Text>}
-        <SimpleGrid columns={{
-            sm:1,md:2,lg:3
-        }} spacing={10} padding={10}>
-            {
-                games.map(game => (
-                    <GameCard game={game}/>
-                ))
-            }
-        </SimpleGrid>
+      <SimpleGrid
+        columns={{
+          sm: 1,
+          md: 2,
+          lg: 3,
+        }}
+        spacing={10}
+        padding={10}
+      >
+        {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {games.map((game) => (
+          <GameCard game={game} />
+        ))}
+      </SimpleGrid>
     </>
   );
 }
