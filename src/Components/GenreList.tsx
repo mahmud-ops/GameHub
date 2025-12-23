58310d70fb69d21f4c2aab6c8413b6a7209d1660
Mahmud <abdullahmahmud01798@gmail.com>
2025-12-23 15:56:31 +0600

    Add loading skeelton for genres

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,12 +1,29 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Text,
+} from "@chakra-ui/react";
 import useGenres from "../Hooks/useGenres";
 import getCroppedImage from "../Services/cropped-image";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading } = useGenres();
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   return (
     <List>
+      {isLoading && skeletons.map(() => (
+        <HStack>
+        <Skeleton marginY={2} boxSize={"50px"} />
+        <SkeletonText width={20} noOfLines={2}/>
+        </HStack>
+      ))}
+
       {data?.map((g) => (
         <ListItem paddingY={2} key={g.id}>
           <HStack>
