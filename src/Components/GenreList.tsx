5dd91ccbeedc93468221488f20efdfa861ea1ae0
Mahmud <abdullahmahmud01798@gmail.com>
2025-12-28 21:38:05 +0600

    Refactor: Move Heading component inside GenreList for better structure

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -21,36 +22,39 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   return (
-    <List>
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <HStack key={skeleton}>
-            <Skeleton marginY={2} boxSize={"50px"} />
-            <SkeletonText width={20} noOfLines={2} />
-          </HStack>
-        ))}
+    <>
+    <Heading fontSize={'3xl'} marginY={3}>Genres</Heading>
+      <List>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <HStack key={skeleton}>
+              <Skeleton marginY={2} boxSize={"50px"} />
+              <SkeletonText width={20} noOfLines={2} />
+            </HStack>
+          ))}
 
-      {data?.map((g) => (
-        <ListItem paddingY={2} key={g.id}>
-          <HStack>
-            <Image
-              src={getCroppedImage(g.image_background)}
-              boxSize={"32px"}
-              borderRadius={8}
-            ></Image>
-            <Button
-              fontWeight={g.id === selectedGenre?.id ? "bold" : "normal"}
-              color ={g.id === selectedGenre?.id ? 'violet' : ''}
-              variant={"link"}
-              fontSize={15}
-              onClick={() => onSelectGenre(g)}
-            >
-              {g.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+        {data?.map((g) => (
+          <ListItem paddingY={2} key={g.id}>
+            <HStack>
+              <Image
+                src={getCroppedImage(g.image_background)}
+                boxSize={"32px"}
+                borderRadius={8}
+              ></Image>
+              <Button
+                fontWeight={g.id === selectedGenre?.id ? "bold" : "normal"}
+                color={g.id === selectedGenre?.id ? "violet" : ""}
+                variant={"link"}
+                fontSize={15}
+                onClick={() => onSelectGenre(g)}
+              >
+                {g.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
