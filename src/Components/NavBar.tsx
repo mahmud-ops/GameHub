4aca3a2472397555e7e406c77c80fa1667ee375d
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2026-01-29 00:35:36 +0600

    Fixed the nav-bar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,32 +3,34 @@ import { MdMenu } from "react-icons/md";
 import logo from "../assets/GameHub_logo.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 interface Props {
   onOpenDrawer: () => void;
 }
 
 const NavBar = ({ onOpenDrawer }: Props) => {
-
   return (
     <HStack justifyContent={"space-between"} padding="10px">
       <HStack>
         <Show below="lg">
           <IconButton
-            boxShadow={'60px'}
-            padding={'10px'}
+            boxShadow={"60px"}
+            padding={"10px"}
             icon={<MdMenu />}
             onClick={onOpenDrawer}
             aria-label="Open menu"
             variant="ghost"
           />
         </Show>
-        <Hide below='md'>
-          <Image src={logo} padding="10px" boxSize="60px"></Image>
+        <Hide below="md">
+          <Link to={"/"}>
+            <Image src={logo} padding="10px" boxSize="60px"></Image>
+          </Link>
         </Hide>
       </HStack>
       <HStack>
-        <SearchInput/>
+        <SearchInput />
         <ColorModeSwitch />
       </HStack>
     </HStack>
--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -2,11 +2,13 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../Store";
+import { useNavigate } from "react-router-dom";
 
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore(selector => selector.setSearchText)
+  const navigate = useNavigate();
 
   return (
     <form
@@ -14,6 +16,7 @@ const SearchInput = () => {
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate('/')
       }}
     >
       <InputGroup>
